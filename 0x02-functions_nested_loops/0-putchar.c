add03e1a3929461b27713480ae28a569a27ddebe
AlaEltigani <alaaltignai@hotmail.com>
2022-10-18 09:42:11 -0700

    t0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (sucess/correct)
+ */
+
+int main(void)
+
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+
+	{
+
+	        putchar(*sh);
+		sh++;
+
+	}
+
+	 putchar('\n');
+
+
+return (0);
+
+}
