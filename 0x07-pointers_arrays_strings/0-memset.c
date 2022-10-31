f5a437549579d456ae229c20bb71af2579f19e58
AlaEltigani <alaaltignai@hotmail.com>
2022-10-31 14:28:30 -0700

    t0.1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte n amount of times
- * @s: pointer that has the address of the memory to fill
- * @b: constant byte that will be written into the momory
- * @n: amount of times the byte will be written into the memory
- *
- * Return: address of the memory where thebyte was written
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -14,8 +13,6 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		s[i] = b;
-	}
 	return (s);
 }
